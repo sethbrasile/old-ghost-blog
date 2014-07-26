150bdd818a917d6c61765f8f3a677cfc14d2b9de
Seth Brasile <seth.brasile@gmail.com>
2014-07-25 19:55:19 -0500

    Update startBlog.sh

--- startBlog.sh
+++ startBlog.sh
@@ -1,1 +1,1 @@
-docker run -e VIRTUAL_HOST=sethbrasile.geekedout.biz --volumes-from blogVol -name blog -d -p 49154:2368 -v ~/blog:/ghost-override dockerfile/ghost
+docker run -e PAGESPEED=1 -e VIRTUAL_HOST=sethbrasile.geekedout.biz --volumes-from blogVol -name blog -d -p 49154:2368 -v ~/blog:/ghost-override dockerfile/ghost
