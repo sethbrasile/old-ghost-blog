3ad2bb5eeaff9b8a9996166f83dd72bde2469f95
Seth Brasile <seth.brasile@gmail.com>
2014-07-25 19:54:39 -0500

    Update startProxy.sh

--- startProxy.sh
+++ startProxy.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:80 -p 443:443 -e PAGESPEED=1 -v /var/run/docker.sock:/tmp/docker.sock -t sethbrasile/nginx-proxy-pagespeed forego start -r
+docker run -d -p 80:80 -p 443:443 -v /var/run/docker.sock:/tmp/docker.sock -t sethbrasile/nginx-proxy-pagespeed forego start -r
